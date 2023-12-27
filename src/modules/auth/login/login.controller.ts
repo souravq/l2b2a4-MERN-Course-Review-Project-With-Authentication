def085578733e1d3f526bfec6e2cb3f5d8be5034
souravq <souravbera515@gmail.com>
2023-12-27 22:32:06 +0530

    Login User Api Done

--- src/modules/auth/login/login.controller.ts
+++ src/modules/auth/login/login.controller.ts
@@ -1,11 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from 'express'
 import { LoginUserService } from './login.service'
+import sendResponse from '../../../utils/sendResponse'
+import httpStatus from 'http-status'
 
 const loginUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const result = await LoginUserService.loginUser(req.body)
     console.log(result)
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: 'User login successful',
+      data: result,
+    })
   } catch (err) {
     next(err)
   }
--- src/modules/auth/login/login.service.ts
+++ src/modules/auth/login/login.service.ts
@@ -3,31 +3,49 @@ import { RegisterUser } from '../registration/registration.model'
 import { TLoginUser } from './login.interface'
 import bcrypt from 'bcrypt'
 import AppError from '../../../error/handleAppError'
+import jwt from 'jsonwebtoken'
 
 const loginUser = async (loginUserData: TLoginUser) => {
   console.log(loginUserData)
 
   //Check the user exist or not
 
-  const isExist = await RegisterUser.findOne({
+  const user = await RegisterUser.findOne({
     username: loginUserData?.username,
   })
 
-  if (!isExist) {
+  if (!user) {
     throw new AppError(httpStatus.FORBIDDEN, 'User not exist.')
   }
 
   // Check Password
   const isPasswordCorrect = await bcrypt.compare(
     loginUserData?.password,
-    isExist?.password,
+    user?.password,
   )
 
   if (!isPasswordCorrect) {
     throw new AppError(httpStatus.NOT_FOUND, 'Password is not correct')
   }
 
-  return true
+  // Creating Data Payload
+  const jsonPayload = {
+    _id: user._id,
+    role: user.role,
+    email: user.email,
+  }
+
+  const accessToken = jwt.sign(jsonPayload, '123456', { expiresIn: '1h' })
+
+  return {
+    user: {
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+    },
+    token: accessToken,
+  }
 }
 
 export const LoginUserService = {
